ca142f8284ac0bba75cecfe0bf7d017b2dd56e0a
LENOVO <bvkmobilereserv@gmal.com>
2022-11-30 15:01:20 +0700

    adding splashscreen

---
+++ app/src/main/java/com/liaudev/jetstories/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.liaudev.jetstories
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+
+/**
+ * Created by Budiman on 30/11/2022.
+ * Email budiliauw87@gmail.com
+ * Github https://github.com/budiliauw87
+ */
+class SplashActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            delay(3000)
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+}

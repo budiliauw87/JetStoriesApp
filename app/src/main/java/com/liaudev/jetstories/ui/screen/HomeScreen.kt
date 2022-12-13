cb7f16c77b915282ea20a6e953bea03cd2ad9737
LENOVO <bvkmobilereserv@gmal.com>
2022-12-13 08:53:03 +0700

    migration pullRefreshState

--- app/src/main/java/com/liaudev/jetstories/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/liaudev/jetstories/ui/screen/HomeScreen.kt
@@ -1,21 +1,20 @@
 package com.liaudev.jetstories.ui.screen
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.liaudev.jetstories.components.StoryItem
 import com.liaudev.jetstories.ui.viewmodel.StoryViewModel
 
@@ -24,6 +23,7 @@ import com.liaudev.jetstories.ui.viewmodel.StoryViewModel
  * budiliauw87.github.io
  * Budiliauw87@gmail.com
  */
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier,
@@ -31,50 +31,41 @@ fun HomeScreen(
 ) {
     val lazyPagingItems = viewModel.storiesPager.collectAsLazyPagingItems()
     val state: LazyListState = rememberLazyListState()
+    val refreshing = lazyPagingItems.loadState.refresh is LoadState.Loading
 
-    SwipeRefresh(
-        state = rememberSwipeRefreshState((lazyPagingItems.loadState.refresh is LoadState.Loading && lazyPagingItems.itemCount > 0)),
-        onRefresh = {
-            lazyPagingItems.refresh() },
-    ) {
-        LazyColumn(
-            state = state,
-            contentPadding = PaddingValues(10.dp),
-            verticalArrangement = Arrangement.spacedBy(5.dp)
-        )
-        {
+    val pullRefreshState = rememberPullRefreshState(refreshing, { lazyPagingItems.refresh() })
+    Box(Modifier.pullRefresh(pullRefreshState)) {
+        LazyColumn(Modifier.fillMaxSize()) {
             items(items = lazyPagingItems) { item ->
                 item?.let {
+
                     StoryItem(it, modifier)
                 }
             }
-            if (lazyPagingItems.loadState.append is LoadState.Loading) {
-                //load status of the next page
-                item {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(50.dp)
-                    ) {
-                        CircularProgressIndicator(modifier = Modifier.align(alignment = Alignment.Center))
+
+            lazyPagingItems.apply {
+                when {
+                    loadState.refresh is LoadState.Loading -> {
+                       Log.e("HomeScreen","State refresh")
+                    }
+                    loadState.append is LoadState.Loading -> { //loading when loadmore
+                        Log.e("HomeScreen","loading item bottom")
+                    }
+                    loadState.refresh is LoadState.Error -> { // error when refresh
+                        val e = lazyPagingItems.loadState.refresh as LoadState.Error
+                        Log.e("HomeScreen",e.error.localizedMessage!!)
+                    }
+
+                    loadState.append is LoadState.Error -> { //error when load more
+                        val e = lazyPagingItems.loadState.append as LoadState.Error
+                        Log.e("HomeScreen",e.error.localizedMessage!!)
                     }
                 }
             }
+
         }
-        if (lazyPagingItems.loadState.refresh is LoadState.Loading) {
-            if (lazyPagingItems.itemCount == 0) {//loading status when the first response page is loaded
-                Box(modifier = Modifier.fillMaxSize()) {
-                    CircularProgressIndicator(modifier = Modifier.align(alignment = Alignment.Center))
-                }
-            }
-        } else if (lazyPagingItems.loadState.refresh is LoadState.Error) {
-            //Load failed error page
-            Box(modifier = Modifier.fillMaxSize()) {
-                Button(modifier = Modifier.align(alignment = Alignment.Center),
-                    onClick = { lazyPagingItems.refresh() }) {
-                    Text(text = "Loading failed! Please try again")
-                }
-            }
-        }
+        PullRefreshIndicator(refreshing, pullRefreshState, Modifier.align(Alignment.TopCenter))
     }
+
+
 }
